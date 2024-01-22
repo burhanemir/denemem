d1fb18f3b57b418511d913196de27aee2778ca5a
ben_burhan <147529798+burhanemir@users.noreply.github.com>
2024-01-22 16:37:14 +0300

    bot2.py

---
+++ bot2.py
@@ -0,0 +1,27 @@
+import discord
+
+# ayricaliklar (intents) değişkeni botun ayrıcalıklarını depolayacak
+intents = discord.Intents.default()
+# Mesajları okuma ayrıcalığını etkinleştirelim
+intents.message_content = True
+# client (istemci) değişkeniyle bir bot oluşturalım ve ayrıcalıkları ona aktaralım
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$hello'):
+        await message.channel.send("Merhaba")
+    elif message.content.startswith('$heh '):
+        if len(message.content) > 4:
+            count_heh = int(message.content[4:])
+    else:
+        count_heh = 5
+        await message.channel.send("he" * count_heh)
+
+client.run("MTE5NjE0Njk1NTk3MDE3MTAwMg.GVbzCv.rfouz8B9TBBd2StuJgQsLYTguuEZfse_hroAEA")
